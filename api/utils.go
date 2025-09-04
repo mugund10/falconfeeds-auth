f270f918eb9d2e7584de422fea53707486917aea
mugund10 <bjmugundhan@gmail.com>
2025-09-04 20:17:51 +0530

    added env functionality

--- api/utils.go
+++ api/utils.go
@@ -3,6 +3,7 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+	"os"
 
 	"github.com/mugund10/falconfeeds-auth/types"
 )
@@ -12,4 +13,12 @@ func EncodeError(w http.ResponseWriter, statusCode int, Content string) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(types.ErrorResponse{Error: Content})
+}
+
+// checks for env values if not present returns fallback
+func GetEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
 }
--- main.go
+++ main.go
@@ -4,11 +4,25 @@ import (
 	"log"
 
 	"github.com/mugund10/falconfeeds-auth/api"
+	"github.com/mugund10/falconfeeds-auth/storage"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 func main() {
-	listenAddress := ":8080"
-	server := api.Newserver(listenAddress)
-	log.Printf("server running on http://0.0.0.0%s", listenAddress)
+	// config init
+	listenAddress := api.GetEnv("LIS_ADDR", "0.0.0.0:8080")
+	dbAddress := api.GetEnv("DB_ADDR", "mongodb://localhost:27017")
+	dbName := api.GetEnv("DB_NAME", "ff")
+	// db init
+	ops := options.Client().ApplyURI(dbAddress)
+	client, err := mongo.Connect(ops)
+	if err != nil {
+		log.Panic("error connecting to db")
+	}
+	dbStore := storage.NewMongoUserStore(client.Database(dbName))
+	// starts server
+	server := api.Newserver(listenAddress, dbStore)
+	log.Printf("server running on http://%s", listenAddress)
 	log.Fatal(server.Start())
 }
