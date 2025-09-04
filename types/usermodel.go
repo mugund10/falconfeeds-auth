f9dbe8a6748945a4801187bf3afeb53da3dffe15
mugund10 <bjmugundhan@gmail.com>
2025-09-04 19:54:59 +0530

    salting done

--- types/usermodel.go
+++ types/usermodel.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/v2/bson"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -15,13 +16,22 @@ type User struct {
 }
 
 // creates a user with time
-func NewUser(name, email, password string) *User {
+func NewUser(name, email, password string) (*User, error) {
+	epass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
 	tt := time.Now().UTC()
-	//todo: salt the password
 	return &User{
 		Name:      name,
 		Email:     email,
-		Password:  password,
+		Password:  string(epass),
 		CreatedAt: tt,
-	}
+	}, nil
+}
+
+// validates password with hash
+func (u *User) ValidatePass(pass string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(pass))
+	return err == nil
 }
